bf243b1eacb009f199d7ac79f2c77d77ac9675b3
sadmanryanriad <sadmanryanriad@gmail.com>
2024-06-18 17:41:00 +0600

    success story grid ready

--- src/SuccessStories/NavigationButtons.jsx
+++ src/SuccessStories/NavigationButtons.jsx
@@ -1,15 +1,15 @@
 import { useSwiper } from "swiper/react";
-import left from "../assets/previous.png";
-import right from "../assets/next.png";
+import left from "../assets/left.png";
+import right from "../assets/right.png";
 
 const NavigationButtons = () => {
     const swiper = useSwiper();
     return (
         <div className="flex gap-3">
-        <button onClick={()=>swiper.slidePrev()} className="swiper-button-prev w-14 border-2 border-black rounded-full p-2">
+        <button onClick={()=>swiper.slidePrev()} className="swiper-button-prev w-14 border-2 border-white rounded-full p-2">
           <img src={left} alt="left arrow" />
         </button>
-        <button onClick={()=>swiper.slideNext} className="swiper-button-next w-14 border-2 border-black rounded-full p-2">
+        <button onClick={()=>swiper.slideNext} className="swiper-button-next w-14 border-2 border-white rounded-full p-2">
           <img src={right} alt="right arrow" />
         </button>
       </div>
--- src/SuccessStories/SuccessStories.jsx
+++ src/SuccessStories/SuccessStories.jsx
@@ -2,8 +2,11 @@ import SuccessSlides from './SuccessSlides';
 
 const SuccessStories = () => {
     return (
-        <div>
-            <SuccessSlides></SuccessSlides>
+        <div className='grid grid-cols-1 md:grid-cols-3 p-7 md:px-0 md:py-24 bg-gradient-to-r from-blue-500 via-blue-700 to-blue-900'>
+            <div className='md:col-span-1'>
+            <h1 className="text-6xl leading-[5rem] font-bold capitalize text-[#36E8E1] md:pl-28 md:pr-10 md:pt-24">Become a part of Our Success Stories</h1>
+            </div>
+            <div className='md:col-span-2'><SuccessSlides></SuccessSlides></div>
         </div>
     );
 };
