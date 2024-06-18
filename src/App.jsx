3b3d975788596ebca44f6a9bb6c08614cfb80abd
sadmanryanriad <sadmanryanriad@gmail.com>
2024-06-18 21:56:27 +0600

    added some gaps between sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import UpcomingEvent from "./upcomingEvent/UpcomingEvent";
 
 import SuccessStories from "./SuccessStories/SuccessStories";
 import MarqueeSection from "./marquee/MarqueeSection";
+import Gap from "./components/Gap";
 
 export default function App() {
   return (
     <>
       <UpcomingEvent></UpcomingEvent>
+      <Gap></Gap>
       <MarqueeSection></MarqueeSection>
+      <Gap></Gap>
       <SuccessStories></SuccessStories>
+      <Gap></Gap>
     </>
   );
 }
---
+++ src/components/Gap.jsx
@@ -0,0 +1,9 @@
+const Gap = () => {
+    return (
+        <div className="my-28">
+            
+        </div>
+    );
+};
+
+export default Gap;
