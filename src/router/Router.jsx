74df7b02059ddd3ef86b649322cc8152ee647c2d
sadmanryanriad <sadmanryanriad@gmail.com>
2024-06-19 11:01:52 +0600

    /admin-dashboard changed to /admin

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
         errorElement: <ErrorPage></ErrorPage>
     },
     {
-        path: "admin-dashboard",
+        path: "admin",
         element: <AdminDashboard></AdminDashboard>,
         errorElement: <ErrorPage></ErrorPage>
     }
